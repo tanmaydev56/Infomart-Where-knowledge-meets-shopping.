2af358aa7c675778939e7a38c45e456ea9030ef9
Tanmay <tanmaysharma763@gmail.com>
2025-02-11 04:15:15 +0530

    eslint off

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ const nextConfig = {
   images: {
     domains: ["cdn.dummyjson.com"],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
